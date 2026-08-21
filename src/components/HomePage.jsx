40ba6f50ebfa6420918e2f2bbe0b71ba4ed1fcf6
Timure228 <arductimur@gmail.com>
2026-08-21 16:14:28 +0200

    feat: add my image on the website / add "more of me" button

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,23 @@ export default function HomePage() {
     return (
         <>
             <div className="diagonal-black"></div>
-            <span className="main-title">Meine Webseite</span>
+            <img className="my-img" src="src/assets/me.png" width={512}/>
+            <button className="animated-button">
+                <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                    ></path>
+                </svg>
+                <span className="text">More About Me</span>
+                <span className="circle"></span>
+                <svg viewBox="0 0 24 24" className="arr-1" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+                    ></path>
+                </svg>
+            </button>
+
+            <span className="main-title">Hello Portfolio!</span>
         </>
     )
 }
