c511696e2e1962d2cf9e6c5ab9f845df3b57fe0c
Timure228 <arductimur@gmail.com>
2026-08-21 15:54:24 +0200

    init commit

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <>
+            <div className="diagonal-black"></div>
+            <span className="main-title">Meine Webseite</span>
+        </>
+    )
+}
