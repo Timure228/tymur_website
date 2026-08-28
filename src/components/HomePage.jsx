ec60d74ec35cf4ffacf4bf8801547688210c935f
Timure228 <arductimur@gmail.com>
2026-08-28 15:43:41 +0200

    feat: upscale my image and add the hover effect on the image

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,58 @@
+import {useRef, useState} from "react";
+
 export default function HomePage() {
+    const ref = useRef(null)
+    const [style, setStyle] = useState({})
+
+
+    const handleMouseMove = (e) => {
+        const el = ref.current
+
+        if (!el) return;
+
+        const rect = el.getBoundingClientRect()
+        const x = (e.clientX - rect.left) / rect.width - 0.5
+        const y = (e.clientY - rect.top) / rect.height - 0.5
+
+        const maxRotate = 15
+        const rotateY = x * maxRotate
+        const rotateX = -y * maxRotate
+
+        setStyle({
+            transform: `perspective(800px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`
+        })
+    }
+    const handleMouseLeave = () => {
+        setStyle({
+            transform: `perspective(800px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`,
+        });
+    };
+
     return (
         <>
             <div className="diagonal-black"></div>
-            <img className="my-img" src="src/assets/me.png" width={512}/>
+            <div
+                ref={ref}
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                    display: "inline-block",
+                    transition: "transform 0.15s ease-out",
+                    willChange: "transform",
+                    ...style,
+                }}
+            >
+                <img
+                    src="src/assets/me.png"
+                    style={{
+                        display: "block",
+                        borderRadius: "12px",
+                        display: "flex",
+                        marginLeft: "500px"
+                    }}
+                    width={700}
+                />
+            </div>
             <button className="animated-button">
                 <svg viewBox="0 0 24 24" className="arr-2" xmlns="http://www.w3.org/2000/svg">
                     <path
@@ -17,6 +67,8 @@ export default function HomePage() {
                     ></path>
                 </svg>
             </button>
+
+
 
             <span className="main-title">Hello Portfolio!</span>
         </>
