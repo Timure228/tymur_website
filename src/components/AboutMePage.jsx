d8276fd1546b91d0ba7b346b8e4b7626aad864ef
Timure228 <arductimur@gmail.com>
2026-09-10 19:53:03 +0200

    feat: add some text about me

--- src/components/AboutMePage.jsx
+++ src/components/AboutMePage.jsx
@@ -7,9 +7,21 @@ export default function AboutMePage() {
                  id="foot_img" src="src/assets/meine_fuesse.png" width={250}/>
             <p style={{position: "absolute", right: "180px", top: "1040px", rotate: "10deg", fontSize: "1.1em"}}>
                 It's ok my feet are here ;)</p>
-            <p>
-                Its a really good definition of me
-            </p>
+
+            <div>
+                <p style={{width: "900px", fontFamily: "MonsterratBold", fontSize: "1.2em"}}>
+                    My Name is Tymur and Im currently studying Informatics at IMS Kantonschule Büelrain.
+                    I've already over 20 Projects built on GitHub, which you can visit under
+                    <a href="https://github.com/Timure228"> this link</a>.
+                </p>
+                <h1>My Background</h1>
+                <p style={{width: "900px", fontFamily: "MonsterratBold", fontSize: "1.2em"}}>
+                    My roots begin in Ukraine.
+                    I have already been living in Switzerland for 5 years and I've learnt quite a lot of things
+                    like german, programming and found new friends.
+                </p>
+                <img src="src/assets/ukraine.png" width={128}/>
+            </div>
         </>
     )
 }
